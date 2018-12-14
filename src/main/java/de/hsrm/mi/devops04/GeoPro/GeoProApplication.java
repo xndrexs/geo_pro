95c163ed69f98c67ecca8159b8ee14e7de57e74f
Andreas Pöhler <andreas.poehler@detim.de>
2018-12-14 12:32:06 +0100

    Initial Commit

---
+++ src/main/java/de/hsrm/mi/devops04/GeoPro/GeoProApplication.java
@@ -0,0 +1,14 @@
+package de.hsrm.mi.devops04.GeoPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoProApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/de/hsrm/mi/devops04/GeoPro/GeoProApplicationTests.java
@@ -0,0 +1,17 @@
+package de.hsrm.mi.devops04.GeoPro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeoProApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
