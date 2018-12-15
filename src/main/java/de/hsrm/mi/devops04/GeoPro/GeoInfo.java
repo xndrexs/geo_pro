23fe5a3ad4d86aa5ef811a7ec349b412f81e442b
Andreas Pöhler <andreas.poehler@detim.de>
2018-12-15 09:52:26 +0100

    Inner classes for nested xml attributes

--- src/main/java/de/hsrm/mi/devops04/GeoPro/GeoInfo.java
+++ src/main/java/de/hsrm/mi/devops04/GeoPro/GeoInfo.java
@@ -1,46 +1,79 @@
 package de.hsrm.mi.devops04.GeoPro;
 
-import lombok.Data;
-
 import javax.xml.bind.annotation.XmlRootElement;
 
-@Data
 @XmlRootElement(name = "ip")
 public class GeoInfo {
-    private String ip;
-    private String isp;
-    private String country;
-    private String town;
 
-    public String getIp() {
-        return ip;
+    public class GeoInfoResults {
+
+        public class GeoInfoResult {
+            private String ip;
+            private String isp;
+            private String city;
+            private String countrycode;
+            private String countryname;
+
+            public String getIp() {
+                return ip;
+            }
+
+            public void setIp(String ip) {
+                this.ip = ip;
+            }
+
+            public String getIsp() {
+                return isp;
+            }
+
+            public void setIsp(String isp) {
+                this.isp = isp;
+            }
+
+            public String getCity() {
+                return city;
+            }
+
+            public void setCity(String city) {
+                this.city = city;
+            }
+
+            public String getCountrycode() {
+                return countrycode;
+            }
+
+            public void setCountrycode(String countrycode) {
+                this.countrycode = countrycode;
+            }
+
+            public String getCountryname() {
+                return countryname;
+            }
+
+            public void setCountryname(String countryname) {
+                this.countryname = countryname;
+            }
+        }
+
+        public GeoInfoResult getResult() {
+            return result;
+        }
+
+        public void setResult(GeoInfoResult result) {
+            this.result = result;
+        }
+
+        private GeoInfoResult result;
+
     }
 
-    public void setIp(String ip) {
-        this.ip = ip;
+    public GeoInfoResults getResults() {
+        return results;
     }
 
-    public String getIsp() {
-        return isp;
+    public void setResults(GeoInfoResults results) {
+        this.results = results;
     }
 
-    public void setIsp(String isp) {
-        this.isp = isp;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
-    public void setTown(String town) {
-        this.town = town;
-    }
+    private GeoInfoResults results;
 }
