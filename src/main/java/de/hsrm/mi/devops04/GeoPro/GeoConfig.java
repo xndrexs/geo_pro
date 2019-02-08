6907075fc5788bb66e374b57b73bb6cb5cf95583
Andreas Pöhler <andreas.poehler@detim.de>
2019-02-08 16:33:45 +0100

    added geo security

--- src/main/java/de/hsrm/mi/devops04/GeoPro/GeoConfig.java
+++ src/main/java/de/hsrm/mi/devops04/GeoPro/GeoConfig.java
@@ -1,6 +1,5 @@
 package de.hsrm.mi.devops04.GeoPro;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -11,12 +10,35 @@ import javax.validation.constraints.Min;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Validated
 @Component
 @Configuration
 @ConfigurationProperties(prefix = "cache")
 public class GeoConfig {
+
+    public Long getMaxAge() {
+        return maxAge;
+    }
+
+    public void setMaxAge(Long maxAge) {
+        this.maxAge = maxAge;
+    }
+
+    public int getMaxEntries() {
+        return maxEntries;
+    }
+
+    public void setMaxEntries(int maxEntries) {
+        this.maxEntries = maxEntries;
+    }
+
+    public List<String> getCountrycodes() {
+        return countrycodes;
+    }
+
+    public void setCountrycodes(List<String> countrycodes) {
+        this.countrycodes = countrycodes;
+    }
 
     @Min(5)
     private Long maxAge;
